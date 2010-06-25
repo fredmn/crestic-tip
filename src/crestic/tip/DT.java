0c2d439156caaa1ada7c3e4eef5c99be4536260c
Frédéric Morain-Nicolier <fmn@gmp-etud68.site57.univ-reims.fr>
2010-06-25 15:30:02 +0200

    Factorisation de code dans LDM

--- src/crestic/tip/DT.java
+++ src/crestic/tip/DT.java
@@ -39,25 +39,25 @@ public class DT {
 		float min = (float) ipFloat.getMin();
 		float max = (float) ipFloat.getMax();
 
-		float step = (max-min)/(cuts+1); // step that defines intervals of cuts
+		float step = (max - min) / ( cuts + 1); // step that defines intervals of cuts
 
 		fpResult = new FloatProcessor(width, height);
 		fpResult.and(0);
 		for (float threshold = step + min; threshold < max; threshold += step) {
 			// for each gray-level cut
 			ipThresholded = new BinaryProcessor(new ByteProcessor(width, height));
-			byte[] ThresholdedPixels=(byte[]) ipThresholded.getPixels();
-			for (int index=0; index<width*height; index++) {
-				if ((ipPixels[index])>threshold) {
-					ThresholdedPixels[index]=(byte) 0xFF;
+			byte[] ThresholdedPixels = (byte[]) ipThresholded.getPixels();
+			for (int index = 0; index < width * height; index++) {
+				if ((ipPixels[index]) > threshold) {
+					ThresholdedPixels[index] = (byte) 0xFF;
 				}
-				else ThresholdedPixels[index]=(byte) 0;
+				else ThresholdedPixels[index] = (byte) 0;
 			}
 
 			FloatProcessor dt = DT.binaryDT(ipThresholded);
 			fpResult.copyBits(dt,0,0,Blitter.ADD); // updates fpResult
 		}
-		fpResult.multiply(1.0/cuts);
+		fpResult.multiply(1.0 / cuts);
 		fpResult.resetMinAndMax(); 
 		return fpResult;
 	}
--- src/crestic/tip/LDM.java
+++ src/crestic/tip/LDM.java
@@ -9,45 +9,25 @@ import ij.process.ImageProcessor;
 
 public class LDM {
 	
-	public static FloatProcessor binaryLDM (BinaryProcessor A, BinaryProcessor B) {
-		FloatProcessor rvdtA = DT.binaryDTtoForeground(A);
-		FloatProcessor rvdtB = DT.binaryDTtoForeground(B);
-
+	public static FloatProcessor ldm(ImageProcessor A, ImageProcessor B, FloatProcessor dtA, FloatProcessor dtB) {
 		// abs_diffAB = |A-B|
 		FloatProcessor abs_diffAB = (FloatProcessor) _diff(A.convertToFloat(), B.convertToFloat());
 
 		// max_dtA_dtB = Max(DT_A, DT_B)
-		FloatProcessor max_dtA_dtB = (FloatProcessor) _max(rvdtA, rvdtB);
+		FloatProcessor max_dtA_dtB = (FloatProcessor) _max(dtA, dtB);
 
-		// rvdm = |A-B| * Max(DT_A, DT_B)
-		FloatProcessor rvldm = (FloatProcessor) _mult(abs_diffAB, max_dtA_dtB);
-		rvldm.resetMinAndMax();
-		return rvldm;
+		// ldm = |A-B| * Max(DT_A, DT_B)
+		FloatProcessor theldm = (FloatProcessor) _mult(abs_diffAB, max_dtA_dtB);
+		theldm.resetMinAndMax();
+		return theldm;
+	}
+	
+	public static FloatProcessor binaryLDM(BinaryProcessor A, BinaryProcessor B) {
+		return ldm(A, B, DT.binaryDTtoForeground(A), DT.binaryDTtoForeground(B));
 	}
 	
 	public static FloatProcessor RVLDM (ImageProcessor A, ImageProcessor B, int cuts) {
 		// todo : Faut-il normaliser A et B pour éviter une explosion des valeurs?
-		FloatProcessor rvdtA = DT.RVDTtoForeground(A, cuts);
-		FloatProcessor rvdtB = DT.RVDTtoForeground(B, cuts);
-
-		// abs_diffAB = |A-B|
-		FloatProcessor abs_diffAB = (FloatProcessor) _diff(A.convertToFloat(), B.convertToFloat());
-
-		// max_dtA_dtB = Max(DT_A, DT_B)
-		FloatProcessor max_dtA_dtB = (FloatProcessor) _max(rvdtA, rvdtB);
-
-		// rvdm = |A-B| * Max(DT_A, DT_B)
-		FloatProcessor rvldm = (FloatProcessor) _mult(abs_diffAB, max_dtA_dtB);
-		rvldm.resetMinAndMax();
-		return rvldm;
-	}
-
-	
-	// todo : faire une LDM générique à la façon clojure :
-	// (defn ldm [img1 img2 dt] (_* (convert-to-float (_diff img1 img2))
-    //                              (_max (dt img1) (dt img2))))
-	
-	// Sol1 : avec interfaces et classes anomymes : type safe!
-	// Sol2 : avec une classe abstraite LDMgen et des implémentations (LDMbin)
-
+		return ldm(A, B, DT.RVDTtoForeground(A, cuts), DT.RVDTtoForeground(B, cuts));
+	}	
 }
